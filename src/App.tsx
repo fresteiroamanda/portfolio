c4598724bebdecac51fd16568747d24ab124316c
amandaFresteiro <amandagoncalveseng@gmail.com>
2024-10-15 16:16:56 -0300

    global configs ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.scss';
+import Header from './components/header/header'; 
+import Hero from './components/hero/hero'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Hero />
+
+        </div>
+    );
+};
 
 export default App;
